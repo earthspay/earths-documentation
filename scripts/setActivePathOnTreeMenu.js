ae66f7e4fba3c3c71d81750deefa1cc9c534355b
sbibik <sbibik@waveplatform.com>
2019-09-11 18:23:13 +0300

    folding menu

--- scripts/setActivePathOnTreeMenu.js
+++ scripts/setActivePathOnTreeMenu.js
@@ -1,10 +1,10 @@
 var pageChangeHandler = function(event) {
-  var bookSummaryElement = document.querySelector('.book-summary');
-  var activeChapterElement =  document.querySelector('.chapter.active');
-  setTimeout(function() {
-    var activeChapterBoundingClientRect = activeChapterElement.getBoundingClientRect();
-    bookSummaryElement.scrollTop = activeChapterBoundingClientRect.top;
-  }, 2000);
-  gitbook.events.unbind("page.change", pageChangeHandler);
+  // var bookSummaryElement = document.querySelector('.book-summary');
+  // var activeChapterElement =  document.querySelector('.chapter.active');
+  // setTimeout(function() {
+  //   var activeChapterBoundingClientRect = activeChapterElement.getBoundingClientRect();
+  //   bookSummaryElement.scrollTop = activeChapterBoundingClientRect.top;
+  // }, 1000);
+  // gitbook.events.unbind("page.change", pageChangeHandler);
 };
 gitbook.events.bind("page.change", pageChangeHandler);
